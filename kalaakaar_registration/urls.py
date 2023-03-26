9780525663f1ca2735f8e46109831d5071e8f11f
Atishkumar98 <atishkumar31518@gmail.com>
2023-03-26 18:14:33 +0530

    Commit_for_Home

--- kalaakaar_registration/urls.py
+++ kalaakaar_registration/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('confirmed_user/',views.confirmation, name= 'confirmed_user'),
     path('privacy_policy/',views.privacy_policy, name= 'privacy_policy'),
     path('login/' , views.loginpage, name = 'loginpage'),
+    path('logout/', views.logoutUser, name='logout'),
     path('home/' , views.home, name = 'loginpage'),
 
     # path('logout/', views.logoutUser, name='logout'),
